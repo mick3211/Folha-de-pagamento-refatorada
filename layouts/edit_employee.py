b20bd2c8f87382a30bf957146148589e9432d59b
Mickael <mickaelfelizardo2008@gmail.com>
2021-09-05 12:42:22 -0300

    listar agendas

--- layouts/edit_employee.py
+++ layouts/edit_employee.py
@@ -1,4 +1,5 @@
 import PySimpleGUI as sg
+from PySimpleGUI.PySimpleGUI import Input
 
 def edit_employee_layout(employee, agendas: list):
 
@@ -6,7 +7,7 @@ def edit_employee_layout(employee, agendas: list):
     info_list = employee.__dict__
     adress_list = employee.adress.__dict__
     agendas_list = ['Não alterar']
-    agendas_list.extend(i.to_string() for i in agendas)
+    agendas_list.extend(f'{e}.{i.to_string()}' for e, i in enumerate(agendas))
 
     return (
         [sg.Frame('Informações pessoais', [
@@ -16,13 +17,15 @@ def edit_employee_layout(employee, agendas: list):
         [sg.Frame('Definições do empregado', [
             [sg.Text('Tipo:')],
             [sg.Combo(['Horista', 'Assalariado', 'Comissionado'], key='type', default_value=TYPES[type(employee).__name__], enable_events=True, readonly=True)],
+            [sg.Text('Salário:')],
+            [sg.Input(f"R$ {info_list['salary']}", disabled=True, size=(10, 1))],
             [sg.Frame('',[
                 [sg.Text('Porcentagem da comissão:', key='t1'), sg.Input(0, size=(3,1), key='comissao')]
             ], key='com', visible=False)],
             [sg.Text('Método de pagamento:')],
             [sg.Combo(['Cheque pelos Correios', 'Cheque em mãos', 'Depósito bancário'], key='paymethod', default_value=info_list['paymethod'], readonly=True)],
             [sg.Text('Agenda de pagamento (aplicado após o próximo pagamento)')],
-            [sg.Combo(agendas_list, agendas_list[0], key='agenda', readonly=True)],
+            [sg.Combo(agendas_list, agendas_list[0], key='agenda', readonly=True, )],
             [sg.Text('Faz parte do sindicato?')],
             [sg.Radio('Sim', group_id='syn', key='syndicate', enable_events=True, default=False if type(employee.syndicate).__name__ == 'NoSyndicate' else True),
                 sg.Radio('Não', group_id='syn', key='not_syndicate', enable_events=True, default=True if type(employee.syndicate).__name__ == 'NoSyndicate' else False)],
