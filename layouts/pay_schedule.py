73dfb32687de547523ee6fd99fdb9fd8f49162ef
Mickael <mickaelfelizardo2008@gmail.com>
2021-09-05 21:23:03 -0300

    seleção por cpf

--- layouts/pay_schedule.py
+++ layouts/pay_schedule.py
@@ -3,6 +3,11 @@ import PySimpleGUI as sg
 
 def pay_schedule_layout(emplo_list: list, date):
 
+    pay_list = list()
+    
+    for emplo in emplo_list:
+        pay_list.append(f'{emplo.name}//R${emplo.salary}//{emplo.paymethod}')
+
     if len(emplo_list) == 0:
         return(
             [sg.Text('Não há funcionários para serem pagos hoje')],
@@ -10,6 +15,7 @@ def pay_schedule_layout(emplo_list: list, date):
         )
     else:
         return(
-            [sg.Text(f'Hoje: {date}', size=(20,6))],
-            [sg.Multiline(f'{emplo.name}\n') for emplo in emplo_list],
+            [sg.Text(f'Hoje: {date}', font=('arial', 15))],
+            [sg.Multiline('\n'.join(pay_list), size=(50, 15), disabled=True)],
+            [sg.Button('Voltar'), sg.Button('Pagar', button_color='green')]
         )
